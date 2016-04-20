caddfa71f25cb3f00324348c9484be9e69416934
joerozo <jrozo@ucsc.edu>
2016-04-20 14:50:47 -0700

    worked on method printRecord() in rbfm.cc adden variables to rbfm.h

--- lab1_codebase/rbf/rbfm.cc
+++ lab1_codebase/rbf/rbfm.cc
@@ -49,7 +49,83 @@ RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Att
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
     return -1;
 }
+/*int main ()
+{
+  std::vector<int> myvector;
+  for (int i=1; i<=5; i++) myvector.push_back(i);
 
+  std::cout << "myvector contains:";
+  for (std::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  return 0;
+}*/
+/*
+void * memcpy ( void * destination, const void * source, size_t num );
+Copy block of memory
+Copies the values of num bytes from the location pointed to by source directly to the memory block pointed to by destination.*/
+/*#define intSize 4
+#define realSize 4
+#define stringSize 4*/
 RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor, const void *data) {
-    return -1;
+    unsigned offset = 0;
+    unsigned vcl;
+    char * type_string; 
+    int type_int;
+    float type_real;
+
+    for(std::vector<Attribute>recordDescriptor::iterator c = recordDescriptor.begin(); c != recordDescriptor.end(); ++c, i++){
+        if(recordDescriptor.type() == TypeInt){
+            if(recordDescriptor.type() != NULL){
+                memcpy(&type_int, ((char*) data + offset), intSize);
+                offset += intSize;
+                cout << "Attribute " << recordDescriptor[i].type() << " (integer): " << type_int << endl;
+                break;
+            }
+            else{
+                cout << "Attribute " << recordDescriptor[i].type() << " (integer): NULL " << endl;
+                break;
+            }
+        }
+        else if(recordDescriptor.type() == TypeVarChar){
+            if(recordDescriptor.type() != NULL){
+                memcpy(&vcl, ((char*) data + offset), stringSize);
+                offset += stringSize;
+                
+                stringVal = (char*) malloc(vcl + 1);
+                memcpy((void*) stringVal, ((char*) data + offset), vcl);
+                stringVal[vcl]="\0";
+                cout << "Attribute " << recordDescriptor[i].type() << " (VarChar): " << stringVal << endl;
+                break;
+            }   
+            else{
+                cout << "Attribute " << recordDescriptor[i].type() << " (VarChar): NULL " << endl;
+                break;
+            }     
+
+        }
+        else if(recordDescriptor.type() == TypeReal){
+            if(recordDescriptor.type() != NULL){
+                memcpy(type_real, ((char*) data + offset), realSize);
+                offset += realSize;
+                cout << "Attribute: " << recordDescriptor[i].type() << "Real: " <<type_real << endl; 
+                break;
+            }
+            else{
+                cout << "Attribute: " << recordDescriptor[i].type() << "Real: NULL" << endl; 
+                break;
+            }    
+        }
+        
+    }
+    }
 }
+
+
+
+
+
+
+
+
