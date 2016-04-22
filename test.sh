1d5833a9bea1e48ff6627b8a8eed3ea689fa02e3
Alec Reid <amreid@ucsc.edu>
2016-04-21 22:29:35 -0700

    script update 1

--- test.sh
+++ test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-unzip $1.zip
-if [ $? -ne 0 ]; then
-    echo "[ERROR] ZIP File Error. Please fix it!"
-    echo
-    exit 1
-fi
-cd $1
 if [ $? -ne 0 ]; then
     echo "[ERROR] The directory structure is not correct. Please fix it!"
     echo
