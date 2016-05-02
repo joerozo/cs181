915c2da854b7c04145b699a5e69489cc0fe7f395
joerozo <jrozo@ucsc.edu>
2016-05-01 19:34:17 -0700

    working on my methods

--- Project 2/codebase/rm/rm.cc
+++ Project 2/codebase/rm/rm.cc
@@ -43,7 +43,21 @@ RC RelationManager::deleteTable(const string &tableName)
 
 RC RelationManager::getAttributes(const string &tableName, vector<Attribute> &attrs)
 {
+  if(tableName.compare(tables.tbl)==0||tableName.compare(columns.tbl)==0){ // tables.tbl should refer to current instantiation of table 
+    attrs =  table.tbl.attributes ; // attrs needs to be set to current tables' attributes --> idk if I am doing this right?
+    cout<< "Successfully Returned Attributes";
+    return 1;
+  }else{
     return -1;
+  }
+
+  // Need to iterate through columns
+  FileHandle fh;
+  FILE * table_file = fopen(tables.tbl.c_str(), "r+");
+
+  if(table_file == NULL) {
+    cout << "Failed to open file" << endl;
+  }
 }
 
 RC RelationManager::insertTuple(const string &tableName, const void *data, RID &rid)
@@ -58,7 +72,11 @@ RC RelationManager::deleteTuple(const string &tableName, const RID &rid)
 
 RC RelationManager::updateTuple(const string &tableName, const void *data, const RID &rid)
 {
-    return -1;
+  FileHandle update;
+  FILE * updateTuple = fopen(tables.tbl.c_str(), "r+");
+
+  vector<Attributes> tableAttrs;
+  return -1;
 }
 
 RC RelationManager::readTuple(const string &tableName, const RID &rid, void *data)
